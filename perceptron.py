e24326d2d426b9e5d9f5811f02bb779bdd540c4c
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-05 20:53:48 -0600

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -21,3 +21,26 @@ class Perceptron:
         self.activation_func = unit_step_func  # Función de activación (escalón unitario)
         self.weights = None      # Pesos (inicialmente indefinidos)
         self.bias = None         # Sesgo (inicialmente indefinido)
+        
+    # Método para entrenar el perceptrón
+    def fit(self, X, y):
+        n_samples, n_features = X.shape  # Obtener el número de muestras y características
+
+        # Inicializar los pesos y el sesgo a 0
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        # Asegurar que las etiquetas sean 0 o 1
+        y_ = np.where(y > 0 , 1, 0)
+
+        # Aprendizaje de los pesos
+        for _ in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                # Cálculo de la salida lineal
+                linear_output = np.dot(x_i, self.weights) + self.bias
+                y_predicted = self.activation_func(linear_output)
+
+                # Regla de actualización del perceptrón
+                update = self.lr * (y_[idx] - y_predicted)
+                self.weights += update * x_i  # Actualización de los pesos
+                self.bias += update           # Actualización del sesgo
