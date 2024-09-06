bd5a930e347b19a1e3b6787a75e95e0a39c6a6e3
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-05 20:44:25 -0600

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -6,3 +6,19 @@
 #Importacion de las librerias 
 import numpy as np
 import matplotlib.pyplot as plt
+
+def euclidean_distance(x1, x2):
+    return np.sqrt(np.sum((x1-x2)**2))
+
+class KMeans:
+
+    def __init__(self, K=5, max_iters=100, plot_steps=False):
+        self.K = K
+        self.max_iters = max_iters
+        self.plot_steps = plot_steps
+
+        # lista de sample de indices para cada cluster
+        self.clusters = [[] for _ in range(self.K)]
+
+        #  El centro de los (mean vector) para cada cluster
+        self.centroids = []
