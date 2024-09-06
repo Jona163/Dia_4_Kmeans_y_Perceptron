65b27ef9597767463265c0abbba8d7e167026341
Jonathan_Hernandez_Mayen <91714580+Jona163@users.noreply.github.com>
2024-09-05 20:43:06 -0600

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -2,3 +2,7 @@
 # Fecha: 05 Septiembre 2024
 # Descripción: Código para una simulación de los Kmeans
 # GitHub: https://github.com/Jona163
+
+#Importacion de las librerias 
+import numpy as np
+import matplotlib.pyplot as plt
